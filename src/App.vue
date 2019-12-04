b9598271280ae00f3543cb24fc85fe7d5a8dcabe
CastJo <34601111+CastJo@users.noreply.github.com>
2019-12-04 11:45:23 +0800

    替换为cli3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
+<<<<<<< Updated upstream
     <img src="./assets/logo.png">
+=======
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+>>>>>>> Stashed changes
     <router-view/>
   </div>
 </template>
 
+<<<<<<< Updated upstream
 <script>
 export default {
   name: 'App'
@@ -12,12 +20,31 @@ export default {
 </script>
 
 <style>
+=======
+<style lang="scss">
+>>>>>>> Stashed changes
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+<<<<<<< Updated upstream
   margin-top: 60px;
+=======
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+>>>>>>> Stashed changes
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+<<<<<<< Updated upstream
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
@@ -13,3 +14,33 @@ export default new Router({
     }
   ]
 })
+=======
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
+>>>>>>> Stashed changes
