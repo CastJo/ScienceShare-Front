16a6abe1d14e6fac8da0a618c14214f2a0a7fca9
CastJo <34601111+CastJo@users.noreply.github.com>
2019-12-13 17:20:06 +0800

    修改navigator,注销

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,44 +1,61 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo pt-1"
-    mode="horizontal"
-    @select="handleSelect"
-  >
-    <el-menu-item index="1">我的主页</el-menu-item>
-    <el-menu-item index="2">我的文献</el-menu-item>
-    <el-menu-item index="3">我的关注</el-menu-item>
-    <el-badge is-dot class="item">
-      <el-menu-item index="4">消息中心</el-menu-item>
-    </el-badge>
-    <el-badge>
-      <el-menu-item index="5">注销</el-menu-item>
-    </el-badge>
-  </el-menu>
+    <el-menu
+            :default-active="activeIndex"
+            @select="handleSelect"
+            class="el-menu-demo pt-1"
+            mode="horizontal"
+    >
+        <el-menu-item index="1">我的主页</el-menu-item>
+        <el-menu-item index="2">我的文献</el-menu-item>
+        <el-menu-item index="3">我的关注</el-menu-item>
+        <el-badge class="item" is-dot>
+            <el-menu-item index="4">消息中心</el-menu-item>
+        </el-badge>
+        <el-badge>
+            <el-menu-item index="5">注销</el-menu-item>
+        </el-badge>
+    </el-menu>
 </template>
 
 <script>
-import circle from "@/assets/logo.png";
-export default {
-  name: "Navigator",
-  data () {
-    return {
-      input: '',
-      circleUrl: circle,
-      activeIndex: "1",
-      activeIndex2: "1"
+    import {delCookie} from '../assets/js/cookie.js'
+    import circle from "@/assets/logo.png";
+
+    export default {
+        name: "Navigator",
+        data() {
+            return {
+                input: '',
+                circleUrl: circle,
+                activeIndex: "1",
+                activeIndex2: "1"
+            };
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+                switch (key) {
+                    case "1":
+                      console.log("yes");
+                        this.$router.push(
+                            '/main'
+                        );
+                        break;
+                    case "5":
+                        delCookie('username');
+                        this.$store.dispatch('quit');
+                        this.$router.replace('/login');
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     };
-  },
-  methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath);
-    }
-  }
-};
 </script>
 
 <style>
-el-menu-item {
-  max-height: 30px;
-}
+    el-menu-item {
+        max-height: 30px;
+    }
 </style>
