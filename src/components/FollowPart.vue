5fdceff39ff735c2f797f08a1f720c8a9b6bab69
CastJo <34601111+CastJo@users.noreply.github.com>
2019-12-19 15:57:09 +0800

    实现关注取关

--- src/components/FollowPart.vue
+++ src/components/FollowPart.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
+        <div>
         <el-avatar :src="this.part.avatarUrl" class="avatar" :size="50"/>
+        </div>
+        <div>
         <span>{{ this.part.username }}</span>
-        <span>{{ this.part.username }}</span>
+        <span>{{ this.part.college }}</span>
+        </div>
+        <div>
+        <el-button @click="follow">关注</el-button>
+            <el-button @click="unFollow">取关</el-button>
+        </div>
     </div>
 </template>
 
@@ -11,6 +19,68 @@
         name: "FollowPart",
         data() {
             return {}
+        },
+        methods:{
+          follow(){
+              this.$axios.get(`usercenter/follow`, {
+                  params: {
+                      followUserName: this.part.username,
+                      myUserName:this.$store.state.user.username,
+                  }
+              }).then((response) => {
+                  switch (response.data.code) {
+                    case 404:
+                        this.$notify.error({
+                            title: "错误",
+                            message: response.data.message,
+                        });
+                        break;
+                    case 201:
+                        this.$notify.error({
+                            title: "错误",
+                            message: response.data.message,
+                        });
+                        break;
+                        case 200:
+                            this.$notify({
+                                title: "成功",
+                                message: response.data.message,
+                                type: "success"
+                            });
+                            break;
+                  }
+              });
+          },
+            unFollow(){
+                this.$axios.get(`usercenter/unFollow`, {
+                    params: {
+                        followUserName: this.part.username,
+                        myUserName:this.$store.state.user.username,
+                    }
+                }).then((response) => {
+                    switch (response.data.code) {
+                        case 404:
+                            this.$notify.error({
+                                title: "错误",
+                                message: response.data.message,
+                            });
+                            break;
+                        case 300:
+                            this.$notify.error({
+                                title: "错误",
+                                message: response.data.message,
+                            });
+                            break;
+                        case 200:
+                            this.$notify({
+                                title: "成功",
+                                message: response.data.message,
+                                type: "success"
+                            });
+                            break;
+                    }
+                });
+            },
         },
         props: ['part',
         ]
