46c48752203aea03fa1fd328dbacc3f8e8719234
huyikun <48144200+huyikun@users.noreply.github.com>
2019-12-19 15:10:33 +0800

    information

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,15 +1,57 @@
 <template>
   <div>
-    <p>{{ msg }}</p>
+    <el-row :gutter="20">
+      <el-col :span="16">
+        <el-card class="my-2 ml-4" shadow="hover">
+          <div slot="header" class="mb-4">
+            <span style="float: left"><b>Information</b></span>
+            <el-button
+              v-if="hasPermission"
+              style="float: right; padding: 3px 0"
+              type="text"
+              @click="dialogVisible = true"
+              >Edit<i class="el-icon-edit"></i
+            ></el-button>
+            <el-dialog
+              title="Edit Your Information"
+              :visible.sync="dialogVisible"
+              :modal-append-to-body="false"
+            >
+            </el-dialog>
+          </div>
+          <div>Institution: {{ this.expertPage.institution }}</div>
+          <div>Degree: {{ this.expertPage.degree }}</div>
+          <div>WebsiteUrl: {{ this.expertPage.webSiteUrl }}</div>
+          <div>Phome: {{ this.expertPage.phone }}</div>
+          <div>Email: {{ this.expertPage.email }}</div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      msg: "information"
+      dialogVisible: false,
+      newInfo: "",
     };
+  },
+  mounted () {
+    console.log(this.$store.state.expertPage)
+  },
+  computed: {
+    expertPage: {
+      get () {
+        return this.$store.state.expertPage
+      }
+    },
+    hasPermission: {
+      get () {
+        return this.$store.state.expertPage.expertName === this.$store.state.user.username
+      }
+    }
   }
 };
 </script>
