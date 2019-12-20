84a5de059c0440944a836127cf085e6feecbe2a3
CastJo <34601111+CastJo@users.noreply.github.com>
2019-12-20 16:14:35 +0800

    收藏夹页面

--- src/components/Favorite.vue
+++ src/components/Favorite.vue
@@ -2,15 +2,60 @@
     <div class="favoriteDiv">
         <el-card style="height: 400px; width: 100%">
             <span>收藏夹</span>
-
+            <div class="checkApply">
+                <ul
+                        class="infinite-list"
+                        style="overflow:auto;list-style: none;padding: 0 0 0 0; max-height: 400px"
+                >
+                    <li
+                            v-for="i in favorites"
+                            class="infinite-list-item"
+                            :key="i.authorId"
+                    >
+                        <FavoritePart :part="i"/>
+                    </li>
+                </ul>
+            </div>
         </el-card>
     </div>
 </template>
 
 <script>
+    import FollowPart from "./FollowPart";
+    import FavoritePart from "./FavoritePart";
     export default {
-        name: "Favorite"
-    }
+        name: "Favorite",
+        components: {FavoritePart, FollowPart},
+        data() {
+            return {
+                favorites: [],
+            };
+        },
+        created() {
+            this.getFavorites();
+        },
+        methods: {
+            getFavorites() {
+                this.$axios
+                    .get("usercenter/getFavorites", {
+                        params: {
+                            username: this.username,
+                        }
+                    })
+                    .then(res => {
+                        if (res.data != null) {
+                            this.favorites = res.data;
+                        }
+                    })
+                    .catch(function(err) {
+                        console.log(err);
+                    });
+            },
+        },
+        props: {
+            username: String
+        }
+    };
 </script>
 
 <style scoped>
---
+++ src/components/FavoritePart.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="userInfoDiv">
+        <div class="nameEnCollege">
+            <div>
+                <h5>{{ this.part.applyUserName }}</h5>
+                <span>{{ this.part.affiliation }}</span>
+            </div>
+        </div>
+        <div class="CheckButton">
+            <el-button type="success" @click="passApplication">通过</el-button>
+            <el-button type="danger" @click="denyApplication">拒绝</el-button>
+        </div>
+        <el-divider style="margin: 12px" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FavoritePart",
+        data() {
+        },
+        created() {
+        },
+        methods: {
+            passApplication(){
+                this.$axios
+                    .get(`usercenter/passApplication`, {
+                        params: {
+                            applyUserName: this.part.applyUserName,
+                            authorId: this.part.authorId
+                        }
+                    })
+                    .then(response => {
+                        switch (response.data.code) {
+                            case 404:
+                                this.$notify.error({
+                                    title: "错误",
+                                    message: response.data.message
+                                });
+                                break;
+                            case 201:
+                                this.$notify.error({
+                                    title: "错误",
+                                    message: response.data.message
+                                });
+                                break;
+                            case 200:
+                                this.$notify({
+                                    title: "成功",
+                                    message: response.data.message,
+                                    type: "success"
+                                });
+                                this.$emit("deleteLine", this.part);
+                                break;
+                        }
+
+                    }).catch(function (err) {
+                    console.log(err);
+                });
+            },
+            denyApplication(){
+                this.$axios
+                    .get(`usercenter/denyApplication`, {
+                        params: {
+                            applyUserName: this.part.applyUserName,
+                            authorId: this.part.authorId
+                        }
+                    })
+                    .then(response => {
+                        switch (response.data.code) {
+                            case 404:
+                                this.$notify.error({
+                                    title: "错误",
+                                    message: response.data.message
+                                });
+                                break;
+                            case 201:
+                                this.$notify.error({
+                                    title: "错误",
+                                    message: response.data.message
+                                });
+                                break;
+                            case 200:
+                                this.$notify({
+                                    title: "成功",
+                                    message: response.data.message,
+                                    type: "success"
+                                });
+                                this.$emit("deleteLine", this.part);
+                                break;
+                        }
+
+                    }).catch(function (err) {
+                    console.log(err);
+                });
+
+            },
+        },
+        props: ["part"]
+    };
+</script>
+
+<style scoped>
+    .userInfoDiv {
+        height: 60px;
+    }
+
+    .userInfoDiv div {
+        float: left;
+    }
+
+    .nameEnCollege {
+        margin-left: 10px;
+        width: 170px;
+        height: 60px;
+    }
+
+    .nameEnCollege div {
+        float: none;
+    }
+
+    .CheckButton {
+        padding-top: 15px;
+        height: 60px;
+    }
+</style>
